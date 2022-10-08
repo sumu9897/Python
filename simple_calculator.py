4d4d176c09cec7f6586ced558fa60263c98d9dbf
MD SUMON <67429465+sumu9897@users.noreply.github.com>
2022-10-08 18:10:20 +0600

    Create simple_calculator.py

---
+++ simple_calculator.py
@@ -0,0 +1,21 @@
+a= int(input("Enter number of a : "))
+b= int(input("Enter number of b : "))
+
+print("SUM=1\nSUB=2\nMUL=3\nDIV=4\n\n")
+c= int(input("Enter number of c : "))
+
+sum = a+b;
+sub = a-b;
+mul = a*b;
+div = a/b;
+
+if(c==1):
+  print("Sum : ",sum)
+elif (c==2):
+  print("Sub : ",sub)
+elif (c==3):
+  print("Mul : ",mul)
+elif (c==4):
+  print("Div : ",div)
+else:
+  print("Invalid Number")
