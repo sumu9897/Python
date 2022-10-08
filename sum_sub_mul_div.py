2df5430286659c4f88e11ee32ca4d5be2e645f50
MD SUMON <67429465+sumu9897@users.noreply.github.com>
2022-10-08 16:43:59 +0600

    Sum , Sub , Mul, Div

---
+++ sum_sub_mul_div.py
@@ -0,0 +1,14 @@
+a= int(input("Enter number of a : "))
+b= int(input("Enter number of b : "))
+
+sum = a+b;
+sub = a-b;
+mul = a*b;
+div = a/b;
+mod =a%b;
+
+print(sum)
+print(sub)
+print(mul)
+print(div)
+print(mod)
