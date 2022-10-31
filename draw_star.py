e0fe95a72a177f90bf18f475d95bd14377d98d5a
MD SUMON <67429465+sumu9897@users.noreply.github.com>
2022-10-31 14:26:05 +0600

    draw a star by using python code
    Here "sumonTurtle.right(144)" 144 comes from 4pi = 4*180=720. These 720 / 5 this answer  144.

---
+++ draw_star.py
@@ -0,0 +1,13 @@
+import turtle
+
+st = turtle.Screen()
+
+sumonTurtle = turtle.Turtle()
+
+# executing loop 5 times for a star
+for i in range(5):
+
+		sumonTurtle.forward(170)
+
+		sumonTurtle.right(144)
+turtle.done()
