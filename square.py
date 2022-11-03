03a72a3a4367bfd8a3a6a2c7668d3bdd8da42d58
MD SUMON <67429465+sumu9897@users.noreply.github.com>
2022-11-03 10:13:00 +0600

    Create square.py

---
+++ square.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.color('red','blue')
+square = turtle.Turtle()
+
+for i in range(4):
+    square.forward(250)
+    square.right(90)#clockwise
+    #square.left(90)#counter colckwise
+
+turtle.done()
