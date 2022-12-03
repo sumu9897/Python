98b033556cb824601af47a02485f14f6f060c857
MD SUMON <67429465+sumu9897@users.noreply.github.com>
2022-12-03 12:54:31 +0600

    Create Comparison.py

---
+++ Comparison.py
@@ -0,0 +1,15 @@
+a= int(input("Enter number of a : "))
+
+b= int(input("Enter number of b : "))
+
+print("a > b  : ",a > b)
+
+print("a < b  : ",a < b)
+
+print("a == b : ",a == b)
+
+print("a != b : ",a != b)
+
+print("a <= b : ",a <= b)
+
+print("a >= b : ",a >= b)
