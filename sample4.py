41fd9e18f6a9ebc3c7a828326c376266d6a36cd0
MD SUMON <67429465+sumu9897@users.noreply.github.com>
2023-01-10 08:46:26 +0600

    Create sample4.py

---
+++ sample4.py
@@ -0,0 +1,10 @@
+"""Maria bought 96 toys priced equally for $12960. The amount of $1015 is still left with her. Find the cost of each toy and the amount she had."""
+
+
+toys_pice=96
+total_toys_price=12960
+left_money=1015
+each_pice_price=(total_toys_price/toys_pice)
+total_money=(total_toys_price+left_money)
+print("Cost of each Toys ",each_pice_price)
+print("Total amount ware ",total_money)
