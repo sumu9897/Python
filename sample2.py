5a6448355fac28160693222b37c0ef029dc2bd39
MD SUMON <67429465+sumu9897@users.noreply.github.com>
2023-01-10 08:39:23 +0600

    Create sample2.py

---
+++ sample2.py
@@ -0,0 +1,8 @@
+"""Linda bought a coat for $2265 and a saree for $2150. She gave $5000 to the shopkeeper. How much money did the shopkeeper return to her?"""
+
+coat_price=2265;
+saree_price=2150;
+cost=coat_price+saree_price;
+given_money=5000;
+return_money=(given_money - cost);
+print("Shopkeeper return $",return_money ,"to Linda")
