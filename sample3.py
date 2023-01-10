0c8fdf11be794ca99b68546de577dcd628f5b14e
MD SUMON <67429465+sumu9897@users.noreply.github.com>
2023-01-10 08:40:28 +0600

    Create sample3.py

---
+++ sample3.py
@@ -0,0 +1,7 @@
+"""A shopkeeper has 2425 boxes of 24 pencils each. How many pencils do all the boxes have in all?"""
+
+
+each_box=24;
+boxes =2425;
+total_pencils=(each_box*boxes)
+print("Total Pencils ",total_pencils)
